df47c2a2d7ea886e6c962def05bfffe94042348b
Hilmi Tekin <hilmi.tekin1@gmail.com>
2019-06-27 23:11:52 +0400

    added k8s support

--- elastic-apm-java/service/src/main/java/com/cosmin/tutorials/apm/tasks/PrintUsersTask.java
+++ elastic-apm-java/service/src/main/java/com/cosmin/tutorials/apm/tasks/PrintUsersTask.java
@@ -1,7 +1,4 @@
 package com.cosmin.tutorials.apm.tasks;
-
-import co.elastic.apm.api.CaptureSpan;
-import co.elastic.apm.api.CaptureTransaction;
 import com.cosmin.tutorials.apm.database.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,28 +17,5 @@ public class PrintUsersTask {
     @Autowired
     public PrintUsersTask(UserRepository userRepository) {
         this.userRepository = userRepository;
-    }
-
-    @Scheduled(fixedDelayString = "5000")
-    public void execute() {
-        logger.info("run scheduled test");
-        doExecute();
-    }
-
-    @CaptureTransaction(type = "Task", value = "PrintUsers")
-    private void doExecute() {
-        userRepository.findAll().forEach(user-> logger.debug(user.getEmail()));
-        sleep();
-    }
-
-    @CaptureSpan("someCustomOperation")
-    private void sleep() {
-        try {
-            Random random = new Random();
-            int milis = random.nextInt(120 - 20 + 1) + 20;
-            Thread.sleep(milis);
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
     }
 }
